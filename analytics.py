5aea5e911ac8639ff1351477f2080a7b8bee3e18
henkva29 <henkva29@penguin>
2020-01-23 14:37:37 +0100

    Sort list

--- analytics.py
+++ analytics.py
@@ -3,34 +3,34 @@ import requests
 import io
 import base64
 from time import sleep
-from nbt import * # !! sudo pip3 install nbt !!
+from nbt import * # !! sudo pip install nbt !!
 
 r = requests.get("https://api.hypixel.net/skyblock/auctions?key=49e3e277-19e8-44e6-9d9d-0230ff58092e&page=0")
 auction_page_json = r.json()
 
-rawBytes="H4sIAAAAAAAAABWNfwqCMBzF36aZG0FH6CxhPyEnZBf4xtYYTA2dUCfyHh4smn++x+d9ngQEmJMAGAd3mu0YVkU3toFLJIFsAnFx2pw82SFSP4m1dsPb01cgvXW9yZctNvP0midfVGVZqRyposZAxLIOvWstJLbHT+hpH2J+jsEM+XKHrH7cr+oMcGQHasiaqMMfWzshTJcAAAA="
 
-
-def decode_inventory_data(raw):
+def ItemDataCount(raw):
    data = nbt.NBTFile(fileobj = io.BytesIO(base64.b64decode(raw)))
-   print(data.pretty_tree()) #tags(0)) #["TAG_Compound"]["Count"].value)
+   
+   return data["i"][0]["Count"]
 
-decode_inventory_data(rawBytes)
+results = []
 
 for i in range(0, auction_page_json["totalPages"]):
     r = requests.get(f"https://api.hypixel.net/skyblock/auctions?key=49e3e277-19e8-44e6-9d9d-0230ff58092e&page={i}")
-    auction = r.json()
-    auction_str = json.dumps(auction["auctions"][0], indent=2)
-    print(auction_str)
+    auctions = r.json()
     
+    for auction in auctions["auctions"]:
+        data = {
+        "name": auction["item_name"],
+        "uuid": auction["uuid"],
+        "count": ItemDataCount(auction["item_bytes"]),
+        "end": auction["end"]
+        }
+        results.append(data)
+    sleep(0.6)
 
-    data = {
-        'item': "brofit",
-        'itemAmount': 1,
-        "auctionUUID": "fefee",
-        "timeStamp": 1579721246009
-    }
+timeSortedResults = sorted() results
 
-    sleep(0.5)
 # auctions_str = json.dumps(auctions_json["auctions"][0], indent=2)
 # print(auctions_str)
