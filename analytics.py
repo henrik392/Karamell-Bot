a48e854327abd8bba2271ac49b05a4f68d8d8dea
henkva29 <henkva29@penguin>
2020-01-24 14:32:44 +0100

    Wow jeg glemte git commit

--- analytics.py
+++ analytics.py
@@ -3,34 +3,56 @@ import requests
 import io
 import base64
 from time import sleep
-from nbt import * # !! sudo pip install nbt !!
-
-r = requests.get("https://api.hypixel.net/skyblock/auctions?key=49e3e277-19e8-44e6-9d9d-0230ff58092e&page=0")
-auction_page_json = r.json()
-
+from nbt import nbt # !! sudo pip install nbt !!
+from datetime import datetime
 
 def ItemDataCount(raw):
    data = nbt.NBTFile(fileobj = io.BytesIO(base64.b64decode(raw)))
    
-   return data["i"][0]["Count"]
+   return int(data["i"][0]["Count"].valuestr())
 
-results = []
-
-for i in range(0, auction_page_json["totalPages"]):
-    r = requests.get(f"https://api.hypixel.net/skyblock/auctions?key=49e3e277-19e8-44e6-9d9d-0230ff58092e&page={i}")
+def AnalyzeAuctions():
+    r = requests.get("https://api.hypixel.net/skyblock/auctions?key=49e3e277-19e8-44e6-9d9d-0230ff58092e&page=0")
     auctions = r.json()
-    
-    for auction in auctions["auctions"]:
-        data = {
-        "name": auction["item_name"],
-        "uuid": auction["uuid"],
-        "count": ItemDataCount(auction["item_bytes"]),
-        "end": auction["end"]
-        }
-        results.append(data)
-    sleep(0.6)
+    results = []
 
-timeSortedResults = sorted() results
+    for i in range(0, auctions["totalPages"]):
+        if i != 0:
+            r = requests.get(f"https://api.hypixel.net/skyblock/auctions?key=49e3e277-19e8-44e6-9d9d-0230ff58092e&page={i}")
+            auctions = r.json()
+        
+        for auction in auctions["auctions"]:
+            data = {
+            "item_name": auction["item_name"],
+            "uuid": auction["uuid"],
+            "count": ItemDataCount(auction["item_bytes"]),
+            "end": round(auction["end"]/1000),
+            "highest_bid": auction["highest_bid_amount"],
+            "starting_bid": auction["starting_bid"]
+            }
 
-# auctions_str = json.dumps(auctions_json["auctions"][0], indent=2)
-# print(auctions_str)
+            results.append(data)
+
+        print(round((i + 1) * 100 / auctions["totalPages"]), "%")
+        sleep(0.5)
+
+    return results
+
+def UpdateAuctionsJson():
+    auctions = AnalyzeAuctions()
+    auctionsByTime = sorted(auctions, key=lambda k: k['end'])
+
+    jsonData = {}
+    jsonData["sorted_auctions"] = auctionsByTime
+    with open('auctions.json', 'w') as outfile:
+        json.dump(jsonData, outfile)
+
+    with open('auctions.json') as infile:
+        auctionResults = json.load(infile)
+
+    for result in auctionResults["sorted_auctions"]:
+        if result["end"] + 300 > datetime.now().timestamp():
+            break
+        print((str(result["count"]) + "x"), result["item_name"], "| highest bid:", result["highest_bid"], "| starting bid:", result["starting_bid"], "timestamp:", result["end"])
+
+UpdateAuctionsJson()
